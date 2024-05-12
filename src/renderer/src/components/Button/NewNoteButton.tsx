b26e358cbf37aea1e7b04da508b2bf14dfc43f40
JotaDD <joaoeduardogomes@gmail.com>
2024-05-12 16:38:16 -0300

    feat: add create and delete note

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,11 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuFileSignature } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreate = () => {
+    createNote()
+  }
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreate} {...props}>
       <LuFileSignature className="w-4 h-4 text-zinc-300" />
-      
     </ActionButton>
   )
 }
