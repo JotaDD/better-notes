3fcb0e248a144d02555123aec19e81fb2a3caffe
JotaDD <joaoeduardogomes@gmail.com>
2024-05-12 15:23:33 -0300

    feat: add notePreview

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -1,6 +1,19 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+const currLanguage = navigator.language
+const currTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+const dateFormatter = new Intl.DateTimeFormat(currLanguage, {
+  dateStyle: 'medium',
+  timeStyle: 'short',
+  timeZone: currTimezone
+})
+
+export const formatDateFromMs = (ms: number) => {
+  return dateFormatter.format(ms)
+}
+
 export const cn = (...args: ClassValue[]) => {
   return twMerge(clsx(...args))
 }
